abca98515d8b4fab11aba1cb8ef2b84cfea69e80
Gaintclout <cloutgaint@gmail.com>
2026-09-02 16:37:27 +0530

    added Ganta Swami in team

--- src/components/Employees/employeeData.js
+++ src/components/Employees/employeeData.js
@@ -6,42 +6,57 @@ const employees = [
     description:
       "Experienced leader in AI program management, driving innovation and strategic initiatives in artificial intelligence projects.",
     image: "/images/Premji.jpeg",
-    cover: "https://images.unsplash.com/photo-1620712943543-bcc4688e7485?q=80&w=1200&auto=format&fit=crop",
-     photoStyle: { objectPosition: "center%", transform: "scale(1.2)" },
+    cover:
+      "https://images.unsplash.com/photo-1620712943543-bcc4688e7485?q=80&w=1200&auto=format&fit=crop",
+    photoStyle: {
+      objectPosition: "center",
+      transform: "scale(1.2)",
+    },
     coverStyle: {
       position: "center 30%",
     },
   },
   {
     id: 2,
+    name: "Ganta Swami",
+    role: "Vice President",
+    description:
+      "Experienced business leader overseeing strategic initiatives, organizational growth, operations, and long-term business development.",
+    image: "/images/GantaSwami.png",
+    cover:
+      "https://images.unsplash.com/photo-1497366811353-6870744d04b2?q=80&w=1200&auto=format&fit=crop",
+    photoStyle: {
+      objectPosition: "center",
+      transform: "scale(1.2)",
+    },
+  },
+  {
+    id: 3,
     name: "Sharath",
     role: "HR Manager",
     description:
       "Experienced HR professional focused on talent acquisition, employee engagement, and building a positive workplace culture.",
     image: "/images/HR.jpg",
-    cover: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d",
-     photoStyle: { objectPosition: "center%", transform: "scale(1.2)" },
-
+    cover:
+      "https://images.unsplash.com/photo-1521737604893-d14cc237f11d",
+    photoStyle: {
+      objectPosition: "center",
+      transform: "scale(1.2)",
+    },
   },
   {
-    id: 3,
-    name: "Shakthi Sharma",
-    role: "Chartered Engineer(India)",
-    description:
-      "Certified engineering professional providing technical evaluation, engineering validation, project certification, and compliance consultancy.",
-    image: "/images/Shakti-sharma.png",
-    cover: "https://images.unsplash.com/photo-1521737604893-d14cc237f11d",
-     photoStyle: { objectPosition: "center%", transform: "scale(1.2)" },
-
-  }, {
-    id: 4,  
+    id: 4,
     name: "Narendra Thari",
     role: "Investor Relations",
     description:
       "Skilled professional in investor relations, managing stakeholder communications, and fostering strong investor relationships.",
     image: "/images/narendra-thari.jpg",
-    cover: "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?q=80&w=1200&auto=format&fit=crop",
-     photoStyle: { objectPosition: "center%", transform: "scale(1.2)" },
+    cover:
+      "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?q=80&w=1200&auto=format&fit=crop",
+    photoStyle: {
+      objectPosition: "center",
+      transform: "scale(1.2)",
+    },
   },
   {
     id: 5,
@@ -50,8 +65,12 @@ const employees = [
     description:
       "Creative designer specializing in user-centered design, branding, and visually engaging digital experiences.",
     image: "/images/Elisha.jpeg",
-    cover: "https://images.unsplash.com/photo-1545235617-9465d2a55698",
-    photoStyle: { objectPosition: "center%", transform: "scale(1.2)" },
+    cover:
+      "https://images.unsplash.com/photo-1545235617-9465d2a55698",
+    photoStyle: {
+      objectPosition: "center",
+      transform: "scale(1.2)",
+    },
   },
   {
     id: 6,
@@ -60,41 +79,54 @@ const employees = [
     description:
       "Skilled cloud engineer with expertise in building secure APIs, managing databases, and scalable server-side systems.",
     image: "/images/Manikanta.jpeg",
-    cover: "https://images.unsplash.com/photo-1555949963-aa79dcee981c",
-    photoStyle: { objectPosition: "center 15%", transform: "scale(1.2)" },
-
+    cover:
+      "https://images.unsplash.com/photo-1555949963-aa79dcee981c",
+    photoStyle: {
+      objectPosition: "center 15%",
+      transform: "scale(1.2)",
+    },
   },
   {
     id: 7,
     name: "Mohammad Umar",
     role: "Associate Software Engineer",
     description:
-      "Passionate Frontend developer focused on building responsive, high-performance UI using React and modern web technologies.",
+      "Passionate frontend developer focused on building responsive, high-performance UI using React and modern web technologies.",
     image: "/images/umar.png",
-    cover: "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
-    photoStyle: { objectPosition: "center 5%", transform: "scale(1.2)", objectFit: "cover", },
-
-  }, {
+    cover:
+      "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+    photoStyle: {
+      objectPosition: "center 5%",
+      transform: "scale(1.2)",
+      objectFit: "cover",
+    },
+  },
+  {
     id: 8,
     name: "Rohini Reddy",
-    role: "Jr.AI Engineer",
+    role: "Jr. AI Engineer",
     description:
       "Skilled AI engineer with expertise in building intelligent systems, machine learning models, and data-driven solutions.",
     image: "/images/rohini.jpeg",
-    cover: "https://images.unsplash.com/photo-1555949963-aa79dcee981c",
-    photoStyle: { objectPosition: "center 60%", transform: "scale(1.2)" },
-
+    cover:
+      "https://images.unsplash.com/photo-1555949963-aa79dcee981c",
+    photoStyle: {
+      objectPosition: "center 60%",
+      transform: "scale(1.2)",
+    },
   },
   {
     id: 9,
     name: "Harshitha Reddy",
-    role: "Jr.AI Engineer",
+    role: "Jr. AI Engineer",
     description:
       "Skilled AI engineer with expertise in building intelligent systems, machine learning models, and data-driven solutions.",
     image: "/images/HarshithaReddy.png",
-    cover: "https://images.unsplash.com/photo-1620712943543-bcc4688e7485?q=80&w=1200&auto=format&fit=crop",
-    photoStyle: { objectPosition: "center 5%", transform: "scale(1.2)" },
-    coverStyle: {
+    cover:
+      "https://images.unsplash.com/photo-1620712943543-bcc4688e7485?q=80&w=1200&auto=format&fit=crop",
+    photoStyle: {
+      objectPosition: "center 5%",
+      transform: "scale(1.2)",
     },
   },
   {
@@ -104,20 +136,27 @@ const employees = [
     description:
       "Skilled professional in data analysis, managing stakeholder communications, and fostering strong data-driven decision-making.",
     image: "/images/Harika.png",
-    cover: "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?q=80&w=1200&auto=format&fit=crop",
-     photoStyle: { objectPosition: "center%", transform: "scale(1.2)" },
-  }, {
+    cover:
+      "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?q=80&w=1200&auto=format&fit=crop",
+    photoStyle: {
+      objectPosition: "center",
+      transform: "scale(1.2)",
+    },
+  },
+  {
     id: 11,
     name: "Maheshwari",
     role: "Data Analyst",
     description:
       "Skilled professional in data analysis, managing stakeholder communications, and fostering strong data-driven decision-making.",
     image: "/images/maheshwari.jpeg",
-    cover: "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?q=80&w=1200&auto=format&fit=crop",
-         photoStyle: { objectPosition: "center%", transform: "scale(1.2)" },
-
-    
-  }
+    cover:
+      "https://images.unsplash.com/photo-1611974789855-9c2a0a7236a3?q=80&w=1200&auto=format&fit=crop",
+    photoStyle: {
+      objectPosition: "center",
+      transform: "scale(1.2)",
+    },
+  },
 ];
 
 export default employees;
